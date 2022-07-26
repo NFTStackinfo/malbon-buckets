ca6fa861f162afc9befb80ac49524e0a8283b4d6
AshotSimonyan <a.simonyan.stdev@gmail.com>
2022-07-26 12:03:52 +0400

    text changes, image changes

--- src/App.js
+++ src/App.js
@@ -12,9 +12,8 @@ const App = () => {
                             <img src="logo.png" alt="Malbon buckets"/>
                         </a>
                     </header>
-                    <h1 className='page-title'>Lorem ipsum dolor sit amet</h1>
-                    <p className='page-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras placerat congue lacus, ut rhoncus
-                        lectus imperdiet sit amet. Vivamus elementum cursus pharetra. </p>
+                    <h1 className='page-title'>Buckets Club Front Nine Membership</h1>
+                    <p className='page-description'>Join our members only golf club by minting one of our tokens. Limited spots.</p>
                     <div className="btn-wrapper">
                         <ConnectButton/>
                     </div>
@@ -22,24 +21,25 @@ const App = () => {
                         <li className='desc-item'>
                             <img src="assets/ether.png" alt=""/>
                             <div className='desc-content'>
-                                <h3 className="desc-title">10,000</h3>
+                                <h3 className="desc-title">9,000</h3>
                                 <p className='desc-text'>NFT membership cards</p>
                             </div>
                         </li>
                         <li className='desc-item'>
                             <img src="assets/wallet.png" alt=""/>
                             <div className='desc-content'>
-                                <h3 className="desc-title">0.05 ETH</h3>
+                                {/*change price every time*/}
+                                <h3 className="desc-title">0.1 ETH</h3>
                                 <p className='desc-text'>Price per NFT</p>
                             </div>
                         </li>
-                        <li className='desc-item'>
+                        {/*<li className='desc-item'>
                             <img src="assets/wallet.png" alt=""/>
                             <div className='desc-content'>
                                 <h3 className="desc-title">500K+</h3>
                                 <p className='desc-text'>Potential opportunity</p>
                             </div>
-                        </li>
+                        </li>*/}
                     </ul>
 
                 </div>
@@ -47,78 +47,78 @@ const App = () => {
             <div className="gallery">
                 <ul className='gallery-row-1 gallery-row'>
                     <li>
-                        <img src={`assets/gallery/1.jpg`} alt=""/>
+                        <img src={`assets/gallery/1.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/2.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/1.jpg`} alt=""/>
+                        <img src={`assets/gallery/3.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/4.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/3.jpg`} alt=""/>
+                        <img src={`assets/gallery/5.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/6.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/1.jpg`} alt=""/>
+                        <img src={`assets/gallery/1.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/3.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/1.jpg`} alt=""/>
+                        <img src={`assets/gallery/9.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/10.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/3.jpg`} alt=""/>
+                        <img src={`assets/gallery/11.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/1.png`} alt=""/>
                     </li>
                 </ul>
                 <ul className='gallery-row-2 gallery-row'>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/11.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/1.jpg`} alt=""/>
+                        <img src={`assets/gallery/10.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/9.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/1.jpg`} alt=""/>
+                        <img src={`assets/gallery/8.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/7.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/3.jpg`} alt=""/>
+                        <img src={`assets/gallery/4.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/5.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/1.jpg`} alt=""/>
+                        <img src={`assets/gallery/6.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/3.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/3.jpg`} alt=""/>
+                        <img src={`assets/gallery/2.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/2.jpg`} alt=""/>
+                        <img src={`assets/gallery/1.png`} alt=""/>
                     </li>
                     <li>
-                        <img src={`assets/gallery/1.jpg`} alt=""/>
+                        <img src={`assets/gallery/11.png`} alt=""/>
                     </li>
                 </ul>
             </div>
